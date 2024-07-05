5e91c0fc18367ddc3065b1fd5ef400a8c4477788
Hugo Lipko <hugo.lipko@gmail.com>
2024-07-05 21:31:50 +0300

    Moved table to component folder and added check for empty localstorage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,42 +1,26 @@
 // import InitialData from "@/data/InitialData.json";
-import data from "../../data/InitialData.json";
+import initialData from "../../data/InitialData.json";
+
+import useStorage from "../../components/Storage";
+import { useState } from "react";
+
+import Table from "../../components/Table";
 
 export default function Home() {
-  window.sessionStorage.setItem("data", JSON.stringify(data));
+  const { setStorage, getStorage, editTableRow, addNewRow, removeRow } =
+    useStorage("data");
 
-  const test = window.sessionStorage.getItem("data");
+  const [tableData, setTableData] = useState(getStorage());
 
-  console.log(JSON.parse(test || ""));
+  if ("data" in localStorage) {
+  } else {
+    setStorage(initialData);
+  }
 
-  function updateCell(newValue: string) {}
   return (
     <main>
       <section>
-        <div className="w-screen h-screen p-40">
-          <table>
-            <tr className="items-start">
-              <th className="w-80 text-start">First Name</th>
-              <th className="w-80 text-start">Last Name</th>
-              <th className="w-80 text-start">E-mail</th>
-              <th className="w-80 text-start">Race</th>
-              <th className="w-80 text-start">IP address</th>
-            </tr>
-
-            {data.map((value) => {
-              return (
-                <tr>
-                  <td>
-                    <input placeholder={value.first_name}></input>
-                  </td>
-                  <td>{value.last_name}</td>
-                  <td>{value.email}</td>
-                  <td>{value.race}</td>
-                  <td>{value.ip_address}</td>
-                </tr>
-              );
-            })}
-          </table>
-        </div>
+        <div className="w-screen h-screen p-40">{Table(tableData)}</div>
       </section>
     </main>
   );
